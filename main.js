deab90da66c1cd198218602cf8a2b779016b62e4
PenguinsSiena <105580668+PenguinsSiena@users.noreply.github.com>
2023-01-11 00:12:36 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+
+}
+function setup(){
+   canvas = createCanvas(350, 300);
+   canvas.center();
+}
+function draw(){
+
+}
+function take_snapshot(){
+    save('MustacheFilter.png');
+}
